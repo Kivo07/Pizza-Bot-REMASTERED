ac9699dade4b4f6033429aaad2e0a5f708bcaef4
Kivo07 <93500087+Kivo07@users.noreply.github.com>
2022-02-06 12:40:21 -0500

    Update main.py
    Added a part of the code that orders the pizza and fixed a bug.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support import ui
+from selenium.webdriver.common.keys import Keys
 
 
 dv = webdriver.Chrome(executable_path=r"C:/Users/Kivanc's Account/Desktop/Selenium Drivers/chromedriver.exe")
 dv.get("https://www.dominos.ca/en/")
+dv.maximize_window()
 
 # Clicking the delivery button
 
@@ -31,7 +33,7 @@ class PizzaBuyer:
 # A person who wants pizza
 
 
-Nathan = PizzaBuyer("123 Somewhere street", "Toronto", "ON", "Postal Code")
+Nathan = PizzaBuyer("731 Sundew Drive", "Waterloo", "ON", "N2K 0B4")
 
 # A function made to fill out the order information
 
@@ -48,3 +50,30 @@ def info():
 
 
 info()
+
+# This function opens up the pizza menu
+
+
+def makepizza():
+    pizza = ui.WebDriverWait(dv, 10).until(ec.element_to_be_clickable((By.ID, "csn-Pizza"))).click()
+
+
+makepizza()
+
+# This function scrolls down to my preferred pizza
+
+# XPATH was used, make sure it is updated regularly
+
+
+def scroll():
+    pepperoni = dv.find_element(By.XPATH, '//*[@id="categoryPage2"]/section/div/div[12]/div/a[1]')
+    pepperoni.send_keys(Keys.PAGE_DOWN)
+    pepperoni.send_keys(Keys.HOME)
+    pepperoni.send_keys(Keys.PAGE_DOWN)
+    pepperoni.click()
+
+
+dv.implicitly_wait(10)
+
+scroll()
+
